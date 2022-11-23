16288a3ad812cea37b6bde690b6361f0c442f687
bgsrio <bgsrio@gmail.com>
2022-11-23 12:33:51 -0300

    home page atualizada com imagem

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Button, Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Corteidh from "../../src/image/Corteidh.png";
 
 const HomePage = () => {
-    return (
-        <Container style={{ height: '100vh' }} className="d-flex align-items-center justify-content-center">
-        <Button className="p-4" variant="dark" size="lg">
-            <Link className="nav-link" to="/reparacoes">
-                InfoAmericano
+  return (
+    <Container
+      style={{ height: "100vh" }}
+      className="d-flex align-items-center justify-content-center"
+    >
+      <Card style={{ width: "30rem" }}>
+        <Card.Img variant="top" src={Corteidh} alt="logoCIDH" />
+        <Card.Body>
+          <Button
+            tag="a"
+            href="#!"
+            className="stretched-link"
+            variant="my-4 text-bg-secondary"
+            size="lg"
+          >
+            <Link className="nav-link" to="/inicial">
+              InfoAmericano
             </Link>
-        </Button>
+          </Button>
+        </Card.Body>
+      </Card>
     </Container>
-    );
+  );
 };
 
 export default HomePage;
